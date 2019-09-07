813f339b945525f5358bac48892549cc6a87f49b
Bhargav Ram <bhargavramkilambi@gmail.com>
2019-09-08 00:42:57 +0530

    simple CRUD + Dockerfile done

---
+++ README.md
@@ -0,0 +1,1 @@
+# A simple Dockerized Exp + MongoDB CRUD application
---
+++ src/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const User = ("./User.model");
+
+const connection = "mongodb://mongo:27017/my-test";
+
+const connectDatabase = () =>{
+    return mongoose.connect(connection);
+};
+
+module.exports = connectDatabase;
